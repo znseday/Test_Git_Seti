d0ed72d419ebbcbe35cf6a23282f5ab80be32fae
Sergey Tsynikin <you@example.com>
2021-10-15 14:38:34 +0300

    Sergey_02 and 01 fixed

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-    cout << "Sergey_01" << endl;
+    cout << "Sergey_01_alpha" << endl;
+    cout << "Sergey_02" << endl;
 
     return 0;
 }
