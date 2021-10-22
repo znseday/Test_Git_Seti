4027b18693942cb0925d7dde4a43dc29b73d05ba
Homyak_zz <blinova_olga_v@mail.ru>
2021-10-22 14:51:35 +0300

    Commit from Olga. Comments added

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ int main()
 {
     cout << "Hello World!" << endl;
     cout << "Sergey_01_alpha" << endl;
-    cout << "Sergey_02" << endl;
+    cout << "Sergey_02" << endl; // Интересная функция
     cout << "Sergey_03" << endl;
 
     cout << "Olga_01" << endl;
+    cout << "Olga_02 from mainOlga" << endl;
 
     return 0;
 }
